4484986790a0c17b5451573d175d60df37f697ec
joaoh3326 <joaoh3326@gmail.com>
2020-02-10 20:58:20 -0300

    adicionando rota de getById mais testes

--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,5 +1,7 @@
 import ProductsController from "../../../src/controllers/products";
 import sinon from "sinon";
+import Product from "../../../src/models/product";
+import { response } from "express";
 
 describe("Controllers: Products", () => {
   const defaultProduct = [
@@ -10,18 +12,62 @@ describe("Controllers: Products", () => {
     }
   ];
 
+  const defaultRequest = {
+    params: {}
+  };
+
   describe("get() products", () => {
-    it("should return a list of products", () => {
-      const request = {};
+    it("should return a list of products", async () => {
       const response = {
         send: sinon.spy()
       };
 
-      const productsController = new ProductsController();
-      productsController.get(request, response);
+      Product.find = sinon.stub();
+      Product.find.withArgs({}).resolves(defaultProduct);
 
-      expect(response.send.called).to.be.true;
-      expect(response.send.calledWith(defaultProduct)).to.be.true;
+      const productsController = new ProductsController(Product);
+      await productsController.get(defaultRequest, response);
+
+      sinon.assert.calledWith(response.send, defaultProduct);
+    });
+
+    it("should return 400 when an error occurs", async () => {
+      const response = {
+        send: sinon.spy(),
+        status: sinon.stub()
+      };
+
+      response.status.withArgs(400).returns(response);
+      Product.find = sinon.stub();
+      Product.find.withArgs({}).rejects({ message: "Error" });
+
+      const productsController = new ProductsController(Product);
+
+      await productsController.get(defaultRequest, response);
+
+      sinon.assert.calledWith(response.send, "Error");
+    });
+  });
+
+  describe("getById()", () => {
+    it("should return one product", async () => {
+      const fakeId = "a-fake-id";
+      const request = {
+        params: {
+          id: fakeId
+        }
+      };
+      const response = {
+        send: sinon.spy()
+      };
+
+      Product.find = sinon.stub();
+      Product.find.withArgs({ _id: fakeId }).resolves(defaultProduct);
+
+      const productsController = new ProductsController(Product);
+      await productsController.getById(request, response);
+
+      sinon.assert.calledWith(response.send, defaultProduct);
     });
   });
 });
